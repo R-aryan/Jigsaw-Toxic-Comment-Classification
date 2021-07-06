1f4588884f393ad50843787dba5ae97a05fed05b
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2021-07-06 20:07:44 +0530

    code for creating Rest API

---
+++ backend/services/toxic_comment_jigsaw/application/configuration.py
@@ -0,0 +1,10 @@
+from injector import Module, singleton
+
+from backend.common.logging.console_loger import ConsoleLogger
+from backend.common.logging.logger import Logger
+
+
+class Configuration(Module):
+    def configure(self, binder):
+        logger = ConsoleLogger()
+        binder.bind(Logger, to=logger, scope=singleton)
